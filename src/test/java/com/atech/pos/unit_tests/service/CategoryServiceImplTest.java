75244e473ef8334c7a59eed15d95f9f9d533b7da
raed <raedgeorge2014@gmail.com>
2025-03-13 12:43:16 +0200

    added base category service test class

---
+++ src/test/java/com/atech/pos/unit_tests/service/CategoryServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.atech.pos.unit_tests.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceImplTest {
+
+    @Test
+    void getAllCategories() {
+    }
+
+    @Test
+    void findCategoryById() {
+    }
+
+    @Test
+    void findCategoryByName() {
+    }
+
+    @Test
+    void createCategory() {
+    }
+
+    @Test
+    void updateCategory() {
+    }
+
+    @Test
+    void deleteCategory() {
+    }
+}
